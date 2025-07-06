47844ecf1dfdd4ae6f7bcb1e82f97d5f15f97b9f
Ravindu Lakruwan <ravindulakruwan79979@gmail.com>
2025-07-06 09:58:34 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,2 +1,2 @@
-self.addEventListener('install', e => self.skipWaiting());
-self.addEventListener('fetch', () => {});
+self.addEventListener("install", (e) => self.skipWaiting());
+self.addEventListener("fetch", () => {});
