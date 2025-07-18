6fdd2cac5f9c426981b366e8de782ba34b3f9f9f
Ravindu Lakruwan <ravindulakruwan79979@gmail.com>
2025-07-18 21:15:41 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,41 +1,43 @@
-const CACHE_NAME = 'fruit-checker-cache-v1';
+// service-worker.js
+const CACHE_NAME = 'fruit-ripeness-detector-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
+  '',
+  'index.html',
   'https://cdn.tailwindcss.com',
   'https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js',
-  // You might want to cache images or other assets if they are static
+  'manifest.json',
+  'icons/icon-192x192.png',
+  'icons/icon-512x512.png'
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => {
+      .then((cache) => {
         console.log('Opened cache');
         return cache.addAll(urlsToCache);
       })
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => {
-        // Cache hit - return response
+      .then((response) => {
         if (response) {
-          return response;
+          return response; // Found in cache
         }
-        return fetch(event.request);
+        return fetch(event.request); // Not in cache, fetch from network
       })
   );
 });
 
-self.addEventListener('activate', event => {
+self.addEventListener('activate', (event) => {
   const cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
-    caches.keys().then(cacheNames => {
+    caches.keys().then((cacheNames) => {
       return Promise.all(
-        cacheNames.map(cacheName => {
+        cacheNames.map((cacheName) => {
           if (cacheWhitelist.indexOf(cacheName) === -1) {
             return caches.delete(cacheName);
           }
